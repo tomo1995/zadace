a66c5f797f9b6e0f0a2d75c8012d42778ff3513f
tomo 1995 <tomislav.trifunovic@bitcamp.ba>
2015-05-24 21:38:16 +0200

    Eighth

---
+++ Koordinate.java
@@ -0,0 +1,17 @@
+
+public class Koordinate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int A = 7;
+		int B = 12;
+		int C = 6;
+		
+		double distance = Math.sqrt(A*A + B*B + C*C);
+		
+		System.out.println("Udaljenost tacke je: " + distance);
+
+	}
+
+}
