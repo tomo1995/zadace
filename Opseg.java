2a548a3742c2afa2f500bd257f59aaf6d88c541d
tomo 1995 <tomislav.trifunovic@bitcamp.ba>
2015-05-24 14:54:33 +0200

    Ovo je moja prva zadaca na Gitu

---
+++ Opseg.java
@@ -0,0 +1,19 @@
+
+public class Opseg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	double a = 5;
+	 if ( a >= 5 && a <= 7.5)
+		 System.out.println( " Varijabla je izmedju 5 i 7.5");
+	 
+	 else if ( a > 7.5 && a  <= 10)
+		 System.out.println( " Varijabla je izmedju 7.5 i 10");
+	 
+	 else
+		 System.out.println( "Varijabla je van opsega");
+
+	}
+
+}
