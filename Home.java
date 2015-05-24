f99e2a2876703c2003ec344a9971c3b5d3249f2a
tomo 1995 <tomislav.trifunovic@bitcamp.ba>
2015-05-24 15:03:53 +0200

    first

---
+++ Home.java
@@ -0,0 +1,25 @@
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int a = 10;
+			int b = 1;
+			int c = 3;
+		
+			if (a + b > c && a + c > b && b + c > a)
+			System.out.println( " Postoji trougao da datim stranicama" ) ;
+			
+		
+			
+			else{
+			
+		
+			System.out.println(" Ne postoji trougao sa datim stranicama");
+			}
+			
+
+	}
+
+}
