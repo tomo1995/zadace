a93d62e5ca574b66a6ba2561a67ce0fee13a8ce6
tomo 1995 <tomislav.trifunovic@bitcamp.ba>
2015-05-24 21:46:28 +0200

    Tenth

---
+++ Variable.java
@@ -0,0 +1,19 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 3;
+		int b = 5;
+		int c = 8;
+		int d = 7;
+		int x = 9;
+		if (a * x * x * x + b * x * x + c * x + d == 0) {
+			System.out.println("Rjesenje je tacno :");
+		} else
+			System.out.println("Rjesenje nije tacno :");
+
+	}
+
+}
